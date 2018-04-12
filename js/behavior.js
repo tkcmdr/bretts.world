05469c13ad55594a8141fe171ae6b107cc692ffe
Brett Hudson <tkcmdr@gmail.com>
2018-04-12 03:23:31 -0400

    Various improvements, cleanup

--- js/behavior.js
+++ js/behavior.js
@@ -1,4 +1,11 @@
-function DisplayContent(content)
+var contentContainer    = document.getElementById("content-container");
+var contentWindow       = document.getElementById("content-window")
+var mainPage            = true;
+
+function ToggleBody(bodyID)
 {
-    console.log("Displaying content!");
+    var body            = document.getElementById(bodyID);
+    var displayMode     = body.style.display=="none"?"block":"none";
+    
+    body.style.display  = displayMode;
 }
--- js/content_loader.js
+++ js/content_loader.js
@@ -1,19 +1,22 @@
 var contentURL = "https://raw.githubusercontent.com/tkcmdr/bretts.world/master/data/content.json";
 var contentContainer = document.getElementById("content-container");
 
-function CreateContentItem(item, side)
+function CreateContentItem(item, isRight, contentID)
 {
     var newHTML = document.createElement("div");
+    var bodyID  = "content-body-" + contentID;
     
     newHTML.innerHTML = "<img class='content-image' style='float:" +
-        side.toLocaleLowerCase() + "' src='img/" +
+        (isRight?"right":"left") + "' src='img/" +
         item.image + "'><h1 class='content-title'>" + 
-        item.title + "</h1>";
-    newHTML.className = "content-item";
-    newHTML.style.animation = "slide" + side + " 1.5s";
+        item.title + "</h1><div id='" + bodyID + 
+        "' class='content-body'>" + item.body + "</div>";
+    newHTML.className       = "content-item";
+    newHTML.id              =  "content-" + contentID;
+    newHTML.style.animation = "slide" + (isRight?"Right":"Left") + " 1.5s";
     newHTML.addEventListener("click", function()
     {
-        DisplayContent(newHTML);
+        ToggleBody(bodyID);
     });
 
     return newHTML;
@@ -28,38 +31,31 @@ function LoadContent()
         var itemCounter     = 1;
         
         responseJSON.forEach(item =>
+        {
+            var isRight = itemCounter%2==0;
+            var contentHTML = CreateContentItem(item, isRight, itemCounter);
+
+            setTimeout(function()
             {
-                var side = itemCounter%2==0 ? "Right" : "Left";
-                var newHTML = CreateContentItem(item, side);
+                contentContainer.appendChild(contentHTML);
+            }, itemCounter * 250);
 
-                setTimeout(function()
-                {
-                    contentContainer.appendChild(newHTML);
-                }, itemCounter * 250);
-
-                itemCounter++;                                
-            });
-        }
+            itemCounter++;                                
+        });
     }
+}
     
-    function XHRRequest(dataType, mode, url, callback, async, data)
-    {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = callback;
-        xhr.responseType = dataType;
-        
-        xhr.open(mode, url, async);
-        xhr.send(data);
-    }
+function XHRRequest(dataType, mode, url, callback, async, data)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onload = callback;
+    xhr.responseType = dataType;
     
-    window.addEventListener("load", function()
-    {
-        XHRRequest(
-            "text",
-            "GET",
-            contentURL,
-            LoadContent,
-            true,
-            null
-        )
-    });
+    xhr.open(mode, url, async);
+    xhr.send(data);
+}
+    
+window.addEventListener("load", function()
+{
+    XHRRequest("text", "GET", contentURL, LoadContent, true)
+});
