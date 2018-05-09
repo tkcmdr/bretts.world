995a2b2d47aebfb2dfd957b1346552f33a76beba
Brett Hudson <tkcmdr@gmail.com>
2018-05-09 01:37:48 -0400

    Clean up JavaScript, improve aesthetics

--- js/content_loader.js
+++ js/content_loader.js
@@ -2,22 +2,41 @@ var articleDir          = "https://raw.githubusercontent.com/tkcmdr/bretts.world
 var contentContainer    = document.getElementById("content-container");
 var taglineElement      = document.getElementById("tagline-element");
 
+// Called when the page content is to be loaded.
+function LoadContent()
+{
+    if (this.status == "200")
+    {
+        var responseJSON    = JSON.parse(this.response);
+        var itemCounter     = 0;
+        
+        // Basically what we're doing here is looping through each article object, and rendering it into existence with a cool animation. Each object is loaded 0.25ms after the one before it, and the image and title are inverted each time for an even neater aesthetic.
+        responseJSON.forEach(item =>
+        {
+            itemCounter++;
+
+            var isRight     = itemCounter%2==0;
+            var isURLTarget = location.hash.substring(1) === item.articleName;
+
+            LoadArticleAfterDelay(
+                item.articleName,
+                CreateContentItem(item, isRight, item.articleName, isURLTarget),
+                itemCounter * 250 // Delay is in milliseconds, so we load in increments of 250ms.
+            );                        
+        });
+    }
+}
+
 // A helper function to create a content element, then return it.
 function CreateContentItem(item, isRight, contentID, isURLTarget)
 {
     var content = document.createElement("div");
-    var image   = document.createElement("img");
     var title   = document.createElement("h1");
     var body    = document.createElement("div");
 
     content.classList       = "content-item article" + (isURLTarget ? " open" : "");
     content.id              =  contentID;
-    content.style.animation = "slide" + (isRight?"Right":"Left") + " 1.5s";
-
-    image.id        = contentID + "-img";
-    image.classList = "content-image";
-    image.style     = "float:" + (isRight?"right":"left");
-    image.src       = item.imagePath;
+    content.style.animation = "slide" + (isRight?"Right":"Left") + " 2s";
 
     title.id        = contentID + "-title";
     title.classList = "content-title";
@@ -26,22 +45,52 @@ function CreateContentItem(item, isRight, contentID, isURLTarget)
     body.id         = contentID + "-body";
     body.classList  = "content-body";
 
-    content.appendChild(image);
-    content.appendChild(title);
-    content.appendChild(body);
-    
-    // Adding event listeners to both the image and title are required, as adding an event listener to the parent element can lead to accidental closing of the content.
     title.addEventListener("click", function()
     {
         ToggleContent(contentID);
     });
-    
-    image.addEventListener("click", function()
+
+    if (item.imagePath != "null")
     {
-        ToggleContent(contentID);
-    });
+        var image   = document.createElement("img");
+
+        image.id        = contentID + "-img";
+        image.classList = "content-image";
+        image.style     = "float:" + (isRight?"right":"left");
+        image.src       = item.imagePath;
+
+        image.addEventListener("click", function()
+        {
+            ToggleContent(contentID);
+        });
+
+        content.appendChild(image);
+    }
+
+    content.appendChild(title);
+    content.appendChild(body);
 
     return content;
+}
+
+// Loads given article (html) after specified delay. The article name is used to find the article path.
+function LoadArticleAfterDelay(name, html, delay)
+{
+    setTimeout(function()
+    {
+        contentContainer.appendChild(html);
+
+        DataRequest(
+            "text",
+            "GET",
+            articleDir + name + ".html",
+            function()
+            {
+                LoadBodyContent(this, html.id + "-body");
+            },
+            true
+        );
+    }, delay);
 }
 
 // Called when the body of the content has been retrieved.
@@ -55,43 +104,7 @@ function LoadBodyContent(xhr, bodyID)
     }
 }
 
-// Called when the page content is to be loaded.
-function LoadContent()
-{
-    if (this.status == "200")
-    {
-        var responseJSON    = JSON.parse(this.response);
-        var itemCounter     = 0;
-        
-        // Basically what we're doing here is looping through each article object, and rendering it into existence with a cool animation. Each object is loaded 0.250ms after the one before it, and the image and title are inverted for an even neater aesthetic.
-        responseJSON.forEach(item =>
-        {
-            itemCounter++;
-
-            var isRight     = itemCounter%2==0;
-            var isURLTarget = location.hash.substring(1) === item.articleName;
-            
-            var contentHTML = CreateContentItem(item, isRight, item.articleName, isURLTarget);
-
-            setTimeout(function()
-            {
-                contentContainer.appendChild(contentHTML);
-
-                DataRequest(
-                    "text",
-                    "GET",
-                    articleDir + item.articleName + ".html",
-                    function()
-                    {
-                        LoadBodyContent(this, contentHTML.id + "-body");
-                    },
-                    true
-                );
-            }, itemCounter * 250);                                
-        });
-    }
-}
-
+// Populates the tagline element with a random tagline from taglines.json.
 function LoadTagline()
 {
     if (this.status == "200")
